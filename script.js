c276c87f40400b785c4614c1a5465ff80a10eaf6
Azhaghiri <ganadhan6528@gmail.com>
2025-06-23 11:04:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function SecondHighest(arr) {
+	let uniqueNumbers = num.filter((num,index) => arr.indexOf(num) === index)
+	.sort((a,b) => b -a)
+	return uniqueNumbers[1]
+}
+console.log(SecondHighestNumber([1, 2, 3, 4, 5])); // Output: 4
